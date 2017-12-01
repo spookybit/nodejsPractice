6c079da0242cec06bf7e2975e16b4856027848fd
Vincent Chan <vincent.chan@biola.edu>
2017-12-01 11:38:28 -0800

    readfile reads html

--- myfirst.js
+++ myfirst.js
@@ -1,14 +1,24 @@
 var http = require('http');
 var dateTime = require('./dateTime');
 var url = require('url');
+var fs = require('fs');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
+
     //res.write("The date and time is " + dateTime.myDateTime())
     //res.write(req.url);
-    let q = url.parse(req.url, true).query;
-    let text = q.year + " " + q.month;
+
+    //let q = url.parse(req.url, true).query;
+    // console.log(q); returns { year: '2017', month: 'July' }
+      // {}
+    //let text = q.year + " " + q.month;
     // http://localhost:8080/?year=2017&month=July returns 2017 July
-    console.log(q);
-    res.end(text);
+    // res.end();
+
+    fs.readFile('demofile1.html', function(err, data) {
+      res.writeHead(200, {'Content-Type':'text/html'});
+      res.write(data);
+      res.end();
+    });
 }).listen(8080);
