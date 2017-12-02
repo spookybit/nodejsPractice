06605ec4da63598dfd64293b185d968a9e40c139
Vincent Chan <vincent.chan@biola.edu>
2017-12-02 08:22:37 -0800

    insertions

---
+++ mongodb.js
@@ -0,0 +1,29 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/mydb";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  // db.createCollection("customers", function(err, res) {
+  //   if (err) throw err;
+  //   db.close()
+  // })
+
+  // var myObj = {name:"company inc", address: "highway 12"};
+  // db.collection("customers").insertOne(myObj, function(err, res) {
+  //   if (err) throw err;
+  //   console.log("inserted");
+  //   db.close();
+  // })
+
+  var myobj = [
+    { _id: 154, name: 'Chocolate Heaven'},
+    { _id: 155, name: 'Tasty Lemon'},
+    { _id: 156, name: 'Vanilla Dream'}
+  ];
+
+  db.collection("products").insertMany(myobj, function(err, res) {
+    if (err) throw err;
+    console.log(res);
+    db.close();
+  });
+});
