56b967caddb7f1d1e7714819bc77e084b1bc0923
Vincent Chan <vincent.chan@biola.edu>
2017-12-03 22:38:19 -0800

    mongodb

--- mongodb.js
+++ mongodb.js
@@ -15,15 +15,24 @@ MongoClient.connect(url, function(err, db) {
   //   db.close();
   // })
 
-  var myobj = [
-    { _id: 154, name: 'Chocolate Heaven'},
-    { _id: 155, name: 'Tasty Lemon'},
-    { _id: 156, name: 'Vanilla Dream'}
-  ];
+  // var myobj = [
+  //   { _id: 154, name: 'Chocolate Heaven'},
+  //   { _id: 155, name: 'Tasty Lemon'},
+  //   { _id: 156, name: 'Vanilla Dream'}
+  // ];
+  //
+  // db.collection("products").insertMany(myobj, function(err, res) {
+  //   if (err) throw err;
+  //   console.log(res);
+  //   db.close();
+  // });
 
-  db.collection("products").insertMany(myobj, function(err, res) {
+  MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  db.collection("products").find({}).toArray(function(err, result) {
     if (err) throw err;
-    console.log(res);
+    console.log(result);
     db.close();
   });
+});
 });
